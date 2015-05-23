6811239672de1a9db02bb40098cd7183eea4bf9f
moutonk <kevin.mouton@epitech.eu>
2015-05-23 15:59:53 +0200

    cubecontrol

--- Hack4Europe/Hack4Europe.Shared/View/ActionsView.xaml.cs
+++ Hack4Europe/Hack4Europe.Shared/View/ActionsView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Hack4Europe.UserControls;
+using Windows.UI.Xaml.Media;
+using Hack4Europe.ViewModel;
 
 namespace Hack4Europe.View
 {
@@ -9,31 +10,40 @@ namespace Hack4Europe.View
         public ActionsView()
         {
             this.InitializeComponent();
+
+        //    Action1.DataContext = new ActionCubeControlViewModel { IconCube = "/CommonAssets/Icons/Temperature Filled-100.png", TitleCube = "temperature", NumberCube = "2", ColorCube = (SolidColorBrush)Application.Current.Resources["ActionGold"] };
+        //    Action2.DataContext = new ActionCubeControlViewModel { IconCube = "/CommonAssets/Icons/IdeaOn-100.png", TitleCube = "lights", NumberCube = "2", ColorCube = (SolidColorBrush)Application.Current.Resources["ActionLime"] };
+        //    Action3.DataContext = new ActionCubeControlViewModel { IconCube = "/CommonAssets/Icons/Air Conditioner-100.png", TitleCube = "air conditionner", ColorCube = (SolidColorBrush)Application.Current.Resources["ActionLightGrey"] };
+        //    Action4.DataContext = new ActionCubeControlViewModel { IconCube = "/CommonAssets/Icons/Heating Room Filled-100.png", TitleCube = "heater", ColorCube = (SolidColorBrush)Application.Current.Resources["ActionDarkOrange"] };
+        //    Action5.DataContext = new ActionCubeControlViewModel { IconCube = "/CommonAssets/Icons/Lamp-100.png", TitleCube = "lamps", ColorCube = (SolidColorBrush)Application.Current.Resources["ActionGreen"] };
+        //    Action6.DataContext = new ActionCubeControlViewModel { IconCube = "/CommonAssets/Icons/Lock 2-100_white.png", TitleCube = "security", ColorCube = (SolidColorBrush)Application.Current.Resources["FlashBlue"] };
+        //    Action7.DataContext = new ActionCubeControlViewModel { IconCube = "/CommonAssets/Icons/Wall Socket-100.png", TitleCube = "wall socket", ColorCube = (SolidColorBrush)Application.Current.Resources["MediumBlue"] };
+        //    Action8.DataContext = new ActionCubeControlViewModel { IconCube = "/CommonAssets/Icons/Motion Detector-100.png", TitleCube = "sensors", ColorCube = (SolidColorBrush)Application.Current.Resources["ActionLightOrange"] };
         }
 
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(GenericActionView), DomoItemControl.DomoItemType.Cooling);
-        }
+        //private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        //{
+        //    Frame.Navigate(typeof(GenericActionView), DomoItemControl.DomoItemType.Cooling);
+        //}
 
-        private void ButtonBase_OnClick1(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(GenericActionView), DomoItemControl.DomoItemType.Heating);
-        }
+        //private void ButtonBase_OnClick1(object sender, RoutedEventArgs e)
+        //{
+        //    Frame.Navigate(typeof(GenericActionView), DomoItemControl.DomoItemType.Heating);
+        //}
 
-        private void ButtonBase_OnClick2(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(GenericActionView), DomoItemControl.DomoItemType.Safety);
-        }
+        //private void ButtonBase_OnClick2(object sender, RoutedEventArgs e)
+        //{
+        //    Frame.Navigate(typeof(GenericActionView), DomoItemControl.DomoItemType.Safety);
+        //}
 
-        private void ButtonBase_OnClick3(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(GenericActionView), DomoItemControl.DomoItemType.Temperature);
-        }
+        //private void ButtonBase_OnClick3(object sender, RoutedEventArgs e)
+        //{
+        //    Frame.Navigate(typeof(GenericActionView), DomoItemControl.DomoItemType.Temperature);
+        //}
 
-        private void ButtonBase_OnClick4(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(GenericActionView), DomoItemControl.DomoItemType.Outlet);
-        }
+        //private void ButtonBase_OnClick4(object sender, RoutedEventArgs e)
+        //{
+        //    Frame.Navigate(typeof(GenericActionView), DomoItemControl.DomoItemType.Outlet);
+        //}
     }
 }
---
+++ Hack4Europe/Hack4Europe.Shared/ViewModel/ActionCubeControlViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Windows.UI.Xaml.Media;
+
+namespace Hack4Europe.ViewModel
+{
+    class ActionCubeControlViewModel : ViewModelBase
+    {
+        private string _iconCube;
+        public string IconCube
+        {
+            get
+            {
+                return _iconCube;
+            }
+            set
+            {
+                NotifyPropertyChanged(ref _iconCube, value);
+            }
+        }
+
+        private string _numberCube;
+        public string NumberCube
+        {
+            get
+            {
+                return _numberCube;
+            }
+            set
+            {
+                NotifyPropertyChanged(ref _numberCube, value);
+            }
+        }
+
+        private string _titleCube;
+        public string TitleCube
+        {
+            get
+            {
+                return _titleCube;
+            }
+            set
+            {
+                NotifyPropertyChanged(ref _titleCube, value);
+            }
+        }
+
+        private SolidColorBrush _colorCube;
+        public SolidColorBrush ColorCube
+        {
+            get
+            {
+                return _colorCube;
+            }
+            set
+            {
+                NotifyPropertyChanged(ref _colorCube, value);
+            }
+        }
+    }
+}
